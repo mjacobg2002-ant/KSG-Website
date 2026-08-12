393ef789c20cb1cb12eb93093ebf6b3be777048c
mjacobg2002-ant <mjacobg2002@gmail.com>
2026-08-12 17:25:11 -0400

    Update Layout.tsx

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,5 +1,4 @@
 import { Outlet, useLocation } from "react-router";
-import { Analytics } from "@vercel/analytics/react";
 import { useEffect } from "react";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
@@ -29,7 +28,6 @@ export function Layout() {
       </main>
       <Footer />
       <FloatingCTA />
-      <Analytics />
     </div>
   );
 }
